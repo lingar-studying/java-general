f0dc74be78e712284add648dce8346edcb6cc2cf
lingar <yimprogramming@gmail.com>
2024-03-04 10:59:19 +0200

    Creating some todo

---
+++ src/swingAndGui/DemoGui.java
@@ -0,0 +1,17 @@
+package swingAndGui;
+//TODO: 
+/**
+ * Do some window that you can insert check boxes, radios, free input. 
+ * 
+ * Take it and retrieve it from some object.
+ * 
+ * Choose some folder and show the files names (run on them). 
+ * 
+ *  in the next - files, and show the
+ */
+public class DemoGui {
+	public static void main(String[] args) {
+		
+	}
+
+}
