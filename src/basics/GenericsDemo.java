6a3fb93c6c5dac44bfcde6d88a2183cb4614d727
lingar <yimprogramming@gmail.com>
2024-03-13 12:44:02 +0200

    Here we see how to make list of different generic types

---
+++ src/basics/GenericsDemo.java
@@ -0,0 +1,42 @@
+package basics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class GenericsDemo {
+
+	public static void main(String[] args) {
+		Object obj1 = new int[2];
+		int x = 5;
+		Object obj2 = 5;
+		Object obj3 = (int)x;
+		
+		//how to make array of some different generics type:
+		
+		 List<String> list = Arrays.asList(new String[] {"One", "Two", "Three"});
+		 System.out.println(Arrays.asList(new int[] {1,2,3}));
+		 Integer [] is = new Integer[] {1,2,3};
+		 List<Integer> list2 = Arrays.asList(is);
+//		 https://docs.oracle.com/javase/tutorial/extra/generics/wildcards.html
+		 List<List<?>> listArr = new ArrayList<>();
+		 listArr.add(list);
+		 listArr.add(list2);
+//		 listArr.add(new Object());//Compile time, you must insert List
+		 
+		 listArr.stream().forEachOrdered(i->System.out.println(i));
+		 System.out.println(listArr.get(1).get(1));
+
+	}
+}
+/*
+About generics an subtyping
+
+https://docs.oracle.com/javase/tutorial/extra/generics/subtype.html
+
+In general, if Foo is a subtype (subclass or subinterface) of Bar,
+ and G is some generic type declaration, it is not the case that G<Foo> is a subtype of G<Bar>. 
+ This is probably the hardest thing you need to learn about generics, because it goes against our deeply held intuitions.
+
+
+*/
